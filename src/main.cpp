ea41232df4610c2577796456de1f479143c322ff
knight <karvin_jimenez@hotmail.com>
2025-02-09 12:30:48 -0400

    project started.

--- src/main.cpp
+++ src/main.cpp
@@ -1,145 +1,106 @@
 #include <raylib.h>
+#include <vector>
 
 const int SCREEN_WIDTH = 960;
 const int SCREEN_HEIGHT = 544;
+
+bool isGamePaused;
 
 typedef struct
 {
     Rectangle bounds;
     Texture2D sprite;
-    int speed;
-    int score;
-} Player;
-
-Player player;
-
-typedef struct
-{
-    Vector2 position;
-    Vector2 velocity;
-    int radius;
-} Ball;
-
-Rectangle ball = {SCREEN_WIDTH / 2 + 50, SCREEN_HEIGHT / 2, 32, 32};
-
-int ballVelocityX = 300;
-int ballVelocityY = 300;
-
-bool isGamePaused;
-
-Sound hitSound;
-
-void update(float deltaTime)
-{
-    if (IsKeyDown(KEY_W) && player.bounds.y >= 0)
-    {
-        player.bounds.y -= player.speed * deltaTime;
-    }
-
-    else if (IsKeyDown(KEY_S) && player.bounds.y <= SCREEN_HEIGHT - player.bounds.height)
-    {
-        player.bounds.y += player.speed * deltaTime;
-    }
-
-    else if (IsKeyDown(KEY_D) && player.bounds.x <= SCREEN_WIDTH - player.bounds.width)
-    {
-        player.bounds.x += player.speed * deltaTime;
-    }
-
-    else if (IsKeyDown(KEY_A) && player.bounds.x > 0)
-    {
-        player.bounds.x -= player.speed * deltaTime;
-    }
-
-    if (ball.x < 0 || ball.x > SCREEN_WIDTH - ball.width)
-    {
-        ballVelocityX *= -1;
-    }
-
-    else if (ball.y < 0 || ball.y > SCREEN_HEIGHT - ball.height)
-    {
-        ballVelocityY *= -1;
-    }
-
-    // Check collision between a two rectangles.
-    if (CheckCollisionRecs(ball, player.bounds))
-    {
-        ballVelocityX *= -1;
-        ballVelocityY *= -1;
-
-        player.score++;
-
-        PlaySound(hitSound);
-    }
-
-    ball.x += ballVelocityX * deltaTime;
-    ball.y += ballVelocityY * deltaTime;
-}
-
-void draw()
-{
-    BeginDrawing();
-
-    ClearBackground(BLACK);
-
-    DrawText(TextFormat("%i", player.score), 230, 20, 80, WHITE);
-
-    DrawTexture(player.sprite, player.bounds.x, player.bounds.y, WHITE);
-
-    DrawRectangleRec(ball, WHITE);
-
-    if (isGamePaused)
-    {
-        DrawText("Game Paused", 220, 100, 80, WHITE);
-    }
-
-    EndDrawing();
-}
+} Kana;
 
 int main()
 {
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Starter");
     SetTargetFPS(60);
 
-    Texture2D sprite = LoadTexture("assets/img/alien.png");
-    player = {{SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, (float)sprite.width, (float)sprite.height}, sprite, 600, 0};
+    Rectangle mouseBounds = {0, 0, 8, 8};
 
-    InitAudioDevice(); // Initialize audio device, before loading sound and music.
+    // need to explicit define local variable values, if not I'll get a segmentation fault.
+    int actualKanaIndex = 0;
+    int score = 0;
 
-    hitSound = LoadSound("assets/sounds/okay.wav");
-    Music music = LoadMusicStream("assets/music/pixel3.mp3");
+    std::vector<Kana> kanas;
+    kanas.reserve(4);
 
-    music.looping = true;
+    Texture2D sprite = LoadTexture("assets/img/ka.png");
+    Kana kana = {{SCREEN_WIDTH / 3, 50, (float)sprite.width, (float)sprite.height}, sprite};
 
-    PlayMusicStream(music);
+    Texture2D sprite2 = LoadTexture("assets/img/ma.png");
+    Kana kana2 = {{SCREEN_WIDTH / 3, 50, (float)sprite2.width, (float)sprite2.height}, sprite2};
+
+    Texture2D sprite3 = LoadTexture("assets/img/wa.png");
+    Kana kana3 = {{SCREEN_WIDTH / 3, 50, (float)sprite3.width, (float)sprite3.height}, sprite3};
+
+    Texture2D sprite4 = LoadTexture("assets/img/to.png");
+    Kana kana4 = {{SCREEN_WIDTH / 3, 50, (float)sprite4.width, (float)sprite4.height}, sprite4};
+
+    Texture2D sprite5 = LoadTexture("assets/img/a.png");
+    Kana kana5 = {{SCREEN_WIDTH / 3, 50, (float)sprite5.width, (float)sprite5.height}, sprite5};
+
+    kanas.push_back(kana);
+    kanas.push_back(kana2);
+    kanas.push_back(kana3);
+    kanas.push_back(kana4);
+    kanas.push_back(kana5);
+
+    InitAudioDevice();
+
+    Sound hitSound = LoadSound("assets/sounds/okay.wav");
 
     while (!WindowShouldClose())
     {
-        UpdateMusicStream(music);
-
-        float deltaTime = GetFrameTime();
-
         if (IsKeyPressed(KEY_SPACE))
         {
             isGamePaused = !isGamePaused;
             PlaySound(hitSound);
         }
 
-        if (!isGamePaused)
+        Vector2 mousePosition = GetMousePosition();
+
+        mouseBounds.x = mousePosition.x;
+        mouseBounds.y = mousePosition.y;
+
+        Kana actualKana = kanas[actualKanaIndex];
+
+        if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT) && CheckCollisionRecs(mouseBounds, actualKana.bounds))
         {
-            update(deltaTime);
+            score++;
+            actualKanaIndex++;
+
+            if (actualKanaIndex > 4)
+            {
+                actualKanaIndex = 0;
+            }
         }
 
-        draw();
+        // if (!isGamePaused)
+        // {
+        // }
+
+        BeginDrawing();
+
+        ClearBackground(Color{29, 29, 27, 255});
+
+        DrawRectangleRounded(actualKana.bounds, 0.3, 6, WHITE);
+        DrawTexture(actualKana.sprite, actualKana.bounds.x, actualKana.bounds.y, WHITE);
+
+        DrawText(TextFormat("%i", score), SCREEN_WIDTH / 2, 10, 20, WHITE);
+
+        if (isGamePaused)
+        {
+            DrawText("Game Paused", 220, 100, 80, WHITE);
+        }
+
+        EndDrawing();
     }
 
-    // Unload texture data
-    UnloadTexture(player.sprite);
+    UnloadTexture(kana.sprite);
 
-    // Unload sound data
     UnloadSound(hitSound);
-    UnloadMusicStream(music);
-
     CloseAudioDevice();
 
     CloseWindow();
