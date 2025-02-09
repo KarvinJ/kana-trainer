3372f806d5d608be32b6cc8d47c51f03a85bfd2e
knight <karvin_jimenez@hotmail.com>
2025-02-09 17:13:53 -0400

    added text box.

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,14 @@
 #include <raylib.h>
 #include <vector>
 #include <iostream>
+#define MAX_INPUT_CHARS 4
 
 const int SCREEN_WIDTH = 960;
 const int SCREEN_HEIGHT = 544;
 
 typedef struct
 {
+    std::string name;
     Rectangle bounds;
     Texture2D texture;
     Sound sound;
@@ -35,7 +37,7 @@ std::vector<Kana> loadAssets()
         Texture2D actualTexture = LoadTexture(actualImagePath.c_str());
         Rectangle kanaBounds = {SCREEN_WIDTH / 3, 50, (float)actualTexture.width, (float)actualTexture.height};
 
-        kanas.push_back({kanaBounds, actualTexture, actualSound});
+        kanas.push_back({kanaName, kanaBounds, actualTexture, actualSound});
     }
 
     return kanas;
@@ -46,10 +48,9 @@ int main()
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Jap-Tester");
     SetTargetFPS(60);
 
-    // bool isGamePaused = false;
-    bool isPracticeMode = false;
+    bool isLearningMode = false;
 
-    // need to explicit define local variable values, if not I'll get a segmentation fault.
+    // need to explicitly define local variable values, if not I'll get a segmentation fault.
     int actualKanaIndex = 0;
     int score = 0;
     float soundTimer = 0;
@@ -60,11 +61,101 @@ int main()
 
     Rectangle mouseBounds = {0, 0, 8, 8};
 
+    char name[MAX_INPUT_CHARS] = "\0"; // NOTE: One extra space required for null terminator char '\0'
+    int letterCount = 0;
+
+    Rectangle textBox = {SCREEN_WIDTH / 3 + 50, 400, 225, 50};
+    bool isMouseOnTextBox = false;
+
+    int framesCounter = 0;
+
     while (!WindowShouldClose())
     {
+        if (CheckCollisionPointRec(GetMousePosition(), textBox))
+        {
+            isMouseOnTextBox = true;
+        }
+        else
+        {
+            isMouseOnTextBox = false;
+        }
+
+        if (isMouseOnTextBox)
+        {
+            // Set the window's cursor to the I-Beam
+            SetMouseCursor(MOUSE_CURSOR_IBEAM);
+
+            // Get char pressed (unicode character) on the queue
+            int key = GetCharPressed();
+
+            // Check if more characters have been pressed on the same frame
+            while (key > 0)
+            {
+                // NOTE: Only allow keys in range [32..125]
+                if ((key >= 32) && (key <= 125) && (letterCount < MAX_INPUT_CHARS))
+                {
+                    name[letterCount] = (char)key;
+                    name[letterCount + 1] = '\0'; // Add null terminator at the end of the string.
+                    letterCount++;
+                }
+
+                key = GetCharPressed(); // Check next character in the queue
+            }
+
+            if (IsKeyPressed(KEY_BACKSPACE))
+            {
+                letterCount--;
+                if (letterCount < 0)
+                {
+                    letterCount = 0;
+                }
+
+                name[letterCount] = '\0';
+            }
+        }
+        else
+        {
+            SetMouseCursor(MOUSE_CURSOR_DEFAULT);
+        }
+
+        if (isMouseOnTextBox)
+        {
+            framesCounter++;
+        }
+        else
+        {
+            framesCounter = 0;
+        }
+
+        Kana actualKana = kanas[actualKanaIndex];
+
+        std::string actualKanaName = name;
+
         if (IsKeyPressed(KEY_SPACE))
         {
-            isPracticeMode = !isPracticeMode;
+            // removing the last character, that is always a blank space.
+            actualKanaName.pop_back();
+            if (actualKana.name.compare(actualKanaName) == 0)
+            {
+                // clearing the array.
+                name[0] = '\0';
+                letterCount = 0;
+                actualKanaIndex = GetRandomValue(0, 4);
+                score++;
+                PlaySound(actualKana.sound);
+            }
+            else
+            {
+                name[0] = '\0';
+                letterCount = 0;
+                actualKanaIndex = GetRandomValue(0, 4);
+                PlaySound(actualKana.sound);
+            }
+        }
+
+        if (IsKeyPressed(KEY_ENTER))
+        {
+            isLearningMode = !isLearningMode;
         }
 
         Vector2 mousePosition = GetMousePosition();
@@ -72,15 +163,11 @@ int main()
         mouseBounds.x = mousePosition.x;
         mouseBounds.y = mousePosition.y;
 
-        Kana actualKana = kanas[actualKanaIndex];
-
         bool hasCollision = CheckCollisionRecs(mouseBounds, actualKana.bounds);
 
-        if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT) && hasCollision)
+        if (isLearningMode && IsMouseButtonPressed(MOUSE_BUTTON_LEFT) && hasCollision)
         {
-            score++;
             actualKanaIndex++;
-            // actualKanaIndex = GetRandomValue(0, 4);
 
             if (actualKanaIndex > 4)
             {
@@ -90,7 +177,7 @@ int main()
 
         soundTimer += GetFrameTime();
 
-        if (!isPracticeMode && soundTimer > 0.6 && (IsMouseButtonPressed(MOUSE_BUTTON_RIGHT) && hasCollision))
+        if (isLearningMode && soundTimer > 0.6 && (IsMouseButtonPressed(MOUSE_BUTTON_RIGHT) && hasCollision))
         {
             PlaySound(actualKana.sound);
             soundTimer = 0;
@@ -103,16 +190,52 @@ int main()
         DrawRectangleRounded({280, 0, 400, SCREEN_HEIGHT}, 0.2, 6, Color{29, 29, 27, 255});
 
         DrawRectangleRounded(actualKana.bounds, 0.3, 6, WHITE);
+
         DrawTexture(actualKana.texture, actualKana.bounds.x, actualKana.bounds.y, WHITE);
 
-        if (isPracticeMode)
+        if (isLearningMode)
         {
-            DrawText("Practice Mode", 400, 10, 24, WHITE);
-            DrawRectangle(SCREEN_WIDTH / 2 - 40, SCREEN_HEIGHT / 2, 70, 40, WHITE);
+            DrawText("Learning Mode", 400, 10, 24, WHITE);
         }
         else
         {
             DrawText(TextFormat("%i", score), SCREEN_WIDTH / 2, 10, 24, WHITE);
+            DrawRectangle(SCREEN_WIDTH / 2 - 40, SCREEN_HEIGHT / 2, 70, 40, WHITE);
+        }
+
+        // drawing text box
+        if (!isLearningMode)
+        {
+            DrawText("PLACE MOUSE OVER INPUT BOX!", SCREEN_WIDTH / 3, 375, 20, LIGHTGRAY);
+
+            DrawRectangleRec(textBox, LIGHTGRAY);
+
+            if (isMouseOnTextBox)
+            {
+                DrawRectangleLines((int)textBox.x, (int)textBox.y, (int)textBox.width, (int)textBox.height, LIGHTGRAY);
+            }
+            else
+            {
+                DrawRectangleLines((int)textBox.x, (int)textBox.y, (int)textBox.width, (int)textBox.height, DARKGRAY);
+            }
+
+            DrawText(name, (int)textBox.x + 5, (int)textBox.y + 8, 40, DARKGRAY);
+
+            if (isMouseOnTextBox)
+            {
+                if (letterCount < MAX_INPUT_CHARS)
+                {
+                    // Draw blinking underscore char
+                    if (((framesCounter / 20) % 2) == 0)
+                    {
+                        DrawText("_", (int)textBox.x + 8 + MeasureText(name, 40), (int)textBox.y + 12, 40, DARKGRAY);
+                    }
+                }
+                else
+                {
+                    DrawText("This is the limit of text allowed", SCREEN_WIDTH / 3, 500, 20, GRAY);
+                }
+            }
         }
 
         EndDrawing();
