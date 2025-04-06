ccd19eff8b38b36a718572ffc01162ecdae957e1
knight <karvin_jimenez@hotmail.com>
2025-04-06 13:12:05 -0400

    refactoring the kana methods in a header.

---
+++ src/assetsManager.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <raylib.h>
+#include <vector>
+#include <iostream>
+
+typedef struct
+{
+    std::string name;
+    Rectangle bounds;
+    Texture2D texture;
+    Sound sound;
+    Texture2D animationTexture;
+    Image image;
+    int animationFrames;
+} Kana;
+
+std::vector<Kana> loadAssets();
+
+std::string handleMissingGifName(std::string kanaName);
